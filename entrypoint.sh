c1c6071cd4d81f4b4ae86c8f7ba7bf73bd483be7
Andreas Nyberg <andreas.nyberg@bra.se>
2025-10-30 17:14:23 +0100

    Use relative path

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 : "${SA_PASSWORD:?SA_PASSWORD is required}"
 
 # Start patiently polling import script in the background
-/usr/src/app/import-data.sh &
+./import-data.sh &
 
 # Start SQL Server in the foreground
 ACCEPT_EULA=Y /opt/mssql/bin/sqlservr
