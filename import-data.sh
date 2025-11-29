ed2cec78f9cd35e44211f97ac4b98cf843f5432a
Andreas Nyberg <andreas.nyberg@bra.se>
2025-11-29 15:56:02 +0100

    Ta bort engelska helt från databasen

--- import-data.sh
+++ import-data.sh
@@ -45,4 +45,5 @@ waitForDb
 run_step setup-schema.ddl done/schema
 run_step setup-sample-metadata.sql done/sample-metadata
 run_step transform-Eng_Sve-to-Sve_Eng.sql done/transform
+run_step drop_Eng.sql done/drop_Eng
 run_step setup-sample-data.sql done/sample-data
