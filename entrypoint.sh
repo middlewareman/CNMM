37f42177ac42e9f11a4b30a431feb3afcb768a95
Andreas Nyberg <andreas.nyberg@bra.se>
2025-12-05 12:52:24 +0100

    Make it a proper #! script; check SA_PASSWORD and set ACCEPT_EULA

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,9 @@
-#start SQL Server, start the script to create the DB and import the data, start the app
-/usr/src/app/import-data.sh & /opt/mssql/bin/sqlservr 
+#!/usr/bin/env bash
+
+: "${SA_PASSWORD:?SA_PASSWORD is required}"
+
+# Start patiently polling import script in the background
+/usr/src/app/import-data.sh &
+
+# Start SQL Server in the foreground
+ACCEPT_EULA=Y /opt/mssql/bin/sqlservr
